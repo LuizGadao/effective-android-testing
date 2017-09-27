28ce62465641def0e3b4a1d0dff1ab18660cd37c
Luiz Carlos <luizgadao@gmail.com>
2017-09-27 16:11:43 -0300

    read file and get params of functional way

--- app/src/main/java/com/sqisland/tutorial/recipes/data/model/Recipe.java
+++ app/src/main/java/com/sqisland/tutorial/recipes/data/model/Recipe.java
@@ -1,12 +1,14 @@
 package com.sqisland.tutorial.recipes.data.model;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Luiz on 27/09/17.
@@ -28,32 +30,29 @@ public class Recipe {
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public static Recipe readFromStream(InputStream inputStream) {
-        String id = null;
-        String title = null;
-        StringBuilder description = new StringBuilder();
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-        try {
-            while (reader.ready()){
-                String line = reader.readLine();
-                if (line.startsWith(ID_PREFIX)) {
-                    id = line.substring(ID_PREFIX.length());
-                    continue;
-                }
-                if (line.startsWith(TITLE_PREFIX)){
-                    title = line.substring(TITLE_PREFIX.length());
-                    continue;
-                }
+        List<String> lines = reader.lines().collect(Collectors.toList());
+        // java.lang.IllegalStateException: stream has already been operated upon or closed
+        String id = getLine(ID_PREFIX, lines);
+        String title = getLine(TITLE_PREFIX, lines);
+        String description = getDescription(lines);
 
-                if (description.length() > 0) description.append("\n");
+        return new Recipe(id, title, description);
+    }
 
-                description.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new Recipe(id, title, description.toString());
+    @SuppressLint("NewApi")
+    private static String getDescription(List<String> lines) {
+        String description = lines.stream().skip(2).collect(Collectors.joining("\n"));
+        return description;
+    }
+
+    @SuppressLint("NewApi")
+    private static String getLine(String prefixLine, List<String> lines) {
+        return lines.stream()
+                .filter(s -> s.startsWith(prefixLine))
+                .map(s -> s.substring(prefixLine.length()))
+                .collect(Collectors.toList()).get(0);
     }
 
 
